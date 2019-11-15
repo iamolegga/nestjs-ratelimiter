46d7dfe5e3d3c053d944a0dc61b989a1ada1567c
Oleg Repin <iamolegga@gmail.com>
2019-11-15 16:19:54 +0200

    add fastify check

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -4,8 +4,9 @@ import {
   DynamicModule,
   Get,
   Module,
+  Type,
 } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { AbstractHttpAdapter, NestFactory } from '@nestjs/core';
 import { RedisModule } from 'nestjs-redis';
 import * as request from 'supertest';
 import {
@@ -14,48 +15,63 @@ import {
   RateLimiterModule,
   RateLimiterParams,
 } from '../src';
+import { fastifyExtraWait } from './utils/fastify-extra-wait';
+import { platforms } from './utils/platforms';
 
-it('should return headers', async () => {
-  const response = await createRequest(
-    RateLimiterModule.forRoot({ getId: () => 'should return headers' }),
-  );
+for (const platform of platforms) {
+  describe(platform.name, () => {
+    const createRequest = CreateRequestFactory(platform);
 
-  expect(response.get('X-RateLimit-Limit')).toBeTruthy();
-  expect(response.get('X-RateLimit-Remaining')).toBeTruthy();
-  expect(response.get('X-RateLimit-Reset')).toBeTruthy();
-});
+    it('should return headers', async () => {
+      const response = await createRequest(
+        RateLimiterModule.forRoot({ getId: () => 'should return headers' }),
+      );
 
-async function createRequest(
-  rmModule: DynamicModule,
-  params?: RateLimiterParams,
+      expect(response.get('X-RateLimit-Limit')).toBeTruthy();
+      expect(response.get('X-RateLimit-Remaining')).toBeTruthy();
+      expect(response.get('X-RateLimit-Reset')).toBeTruthy();
+    });
+  });
+}
+
+function CreateRequestFactory(
+  Platform: Type<AbstractHttpAdapter<any, any, any>>,
 ) {
-  const handlerDecorator = params
-    ? applyDecorators(RateLimiter(params), Get())
-    : Get();
+  return async function createRequest(
+    rmModule: DynamicModule,
+    params?: RateLimiterParams,
+  ) {
+    const handlerDecorator = params
+      ? applyDecorators(RateLimiter(params), Get())
+      : Get();
 
-  @Controller('/')
-  class TestController {
-    @handlerDecorator
-    get() {
-      return {};
+    @Controller('/')
+    class TestController {
+      @handlerDecorator
+      get() {
+        return {};
+      }
     }
-  }
 
-  // tslint:disable-next-line: max-classes-per-file
-  @Module({
-    imports: [rmModule, RedisModule.register({})],
-    controllers: [TestController],
-  })
-  class TestModule {}
+    // tslint:disable-next-line: max-classes-per-file
+    @Module({
+      imports: [rmModule, RedisModule.register({})],
+      controllers: [TestController],
+    })
+    class TestModule {}
 
-  const app = await NestFactory.create(TestModule, { logger: false });
-  app.useGlobalGuards(app.get(RATELIMITER_GUARD_TOKEN));
+    const app = await NestFactory.create(TestModule, new Platform(), {
+      logger: false,
+    });
+    app.useGlobalGuards(app.get(RATELIMITER_GUARD_TOKEN));
 
-  const server = app.getHttpServer();
+    const server = app.getHttpServer();
 
-  await app.init();
-  const response = await request(server).get('/');
-  await app.close();
+    await app.init();
+    await fastifyExtraWait(Platform, app);
+    const response = await request(server).get('/');
+    await app.close();
 
-  return response;
+    return response;
+  };
 }
