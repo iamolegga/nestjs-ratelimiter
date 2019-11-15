5b060a4abb2764d8f295e1e9c4bdbcfc5ce69db5
Oleg Repin <iamolegga@gmail.com>
2019-11-15 18:00:15 +0200

    decorator fix

--- src/decorator.ts
+++ src/decorator.ts
@@ -2,6 +2,6 @@ import { SetMetadata } from '@nestjs/common';
 import { DECORATOR_PARAMS_TOKEN } from './tokens';
 import { RateLimiterParams } from './types';
 
-export function RateLimiter(params: RateLimiterParams) {
+export function RateLimiter(...params: RateLimiterParams[]) {
   return SetMetadata(DECORATOR_PARAMS_TOKEN, params);
 }
